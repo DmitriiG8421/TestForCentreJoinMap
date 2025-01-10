1d861dd3ebdd878f4a39a449fe4f31365782bef9
DmitriiG8421 <golikov.dima15@gmail.com>
2025-01-10 23:17:57 +1100

    Solved

--- NameAgeCity.py
+++ NameAgeCity.py
@@ -1,2 +1,8 @@
-columnsTitles = "   Name   |   Age   |   City   "
+columnsTitles = "Name   |   Age   |   City"
+columns = ["name", "age", "city"]
+
+
+x = list(map(lambda  a : input("Please type your "+ a +": "),columns))
+
 print(columnsTitles.center(len(columnsTitles)+25,"-"))
+print("   |   ".join(x).center(len(columnsTitles)+25,"-"))
